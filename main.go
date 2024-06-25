2ed78733cf106658e3517e6aea0f28b357852612
Ivan Sandrini <ivansandrini@gmail.com>
2024-06-24 21:36:09 -0300

    Encode example

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
 
 type person struct {
@@ -32,4 +33,21 @@ func main() {
 	}
 
 	fmt.Println("back into a Go data structure", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := person{First: "James"}
+
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Encoded bad data", err)
+	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
 }

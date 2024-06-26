52095c018b8ee9c59f3ca7fc5303c152f5f12bc7
Ivan Sandrini <ivansandrini@gmail.com>
2024-06-25 21:43:16 -0300

    Hmac functions

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"fmt"
 	"log"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
+const key = []byte{}
+
 func main() {
+	for i := 1; i < 64; i++ {
+		key = append(key, byte(i))
+	}
 	pass := "12345"
 	hashedPass, err := hashPassword(pass)
 	if err != nil {
@@ -35,3 +43,24 @@ func comparePasswords(password string, hashedPass []byte) error {
 	}
 	return nil
 }
+
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(sha256.New, key)
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("Error while signing message: %v", err)
+	}
+
+	signed := h.Sum(nil)
+	return signed, nil
+}
+
+func checkSign(msg, sig []byte) (bool, error) {
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("Error while checking signature: %v", err)
+	}
+
+	same := hmac.Equal(newSig, sig)
+	return same, nil
+}	
